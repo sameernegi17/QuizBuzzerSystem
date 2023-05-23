8c600812abd2dbceba366ddb1392c36ca8b1e694
Nikolas Paripovic <nikolas.paripovic@esrlabs.com>
2023-05-23 18:53:11 +0200

    Implemented first game mechanics

--- web-server/src/typescript/di.ts
+++ web-server/src/typescript/di.ts
@@ -1,7 +1,10 @@
 import { HttpService } from "./http.service.js"
+import { QuestionService } from "./question.service.js";
 
 const httpService = new HttpService();
+const questionService = new QuestionService();
 
 export default {
   httpService,
+  questionService,
 }
