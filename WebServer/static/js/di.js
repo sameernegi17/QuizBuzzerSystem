406757a7ebb4b59f96bb5865aafb9bf5e9b7c362
Nikolas Paripovic <nikolas.paripovic@esrlabs.com>
2023-05-23 19:55:31 +0200

    Removed old files

--- WebServer/static/js/di.js
+++
@@ -1,8 +0,0 @@
-import { HttpService } from "./http.service.js";
-import { QuestionService } from "./question.service.js";
-var httpService = new HttpService();
-var questionService = new QuestionService();
-export default {
-    httpService: httpService,
-    questionService: questionService,
-};
