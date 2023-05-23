ce6d819f7cbd2eadabe766a8142e36dee932c83a
Nikolas Paripovic <nikolas.paripovic@esrlabs.com>
2023-05-23 16:12:55 +0200

    Reimplemented JS to typescript

--- WebServer/src/typescript/scorepage.ts
+++ WebServer/src/typescript/scorepage.ts
@@ -1,34 +1,39 @@
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( );
-    console.log(JSON.parse(xmlHttp.responseText));
-    return xmlHttp.responseText;
+import di from "./di.js";
+import { HttpService } from "./http.service.js";
+
+class ScorepageComponent {
+
+  constructor(private _httpService: HttpService) {}
+
+  public onInit() {
+    const root = document.getElementById("root");
+    const url = `${this._httpService.getHostBaseUrl()}/score_page`
+    const events = this._httpService.httpGet(url);
+    const obj = JSON.parse(events);
+    const body = document.body,
+    tbl = document.createElement('table');
+    tbl.style.width = '100px';
+    tbl.style.border = '1px solid black';
+    tbl.cellPadding = "40px"
+
+    Object.keys(obj).forEach(function(key) {
+      const tr = tbl.insertRow();
+      const td = tr.insertCell();
+      td.appendChild(document.createTextNode(key))
+      const td1 = tr.insertCell();
+      td1.appendChild(document.createTextNode(obj[key]))
+    })
+  
+    setTimeout(() => {
+      document.location.reload();
+    }, 3000);
+    
+    body.appendChild(tbl);
+    console.log("TEST")
+  }
+
 }
 
-let root = document.getElementById("root");
-let currentHost = window.location.hostname;
-let currentPort = window.location.port;
-let events = httpGet(`http://${currentHost}:${currentPort}/score_page`);
-const obj = JSON.parse(events);
-const body = document.body,
-tbl = document.createElement('table');
-tbl.style.width = '100px';
-tbl.style.border = '1px solid black';
-tbl.cellPadding = "40px"
+const scorepageComponent = new ScorepageComponent(di.httpService);
+scorepageComponent.onInit();
 
-Object.keys(obj).forEach(function(key) {
-    const tr = tbl.insertRow();
-    const td = tr.insertCell();
-    td.appendChild(document.createTextNode(key))
-    const td1 = tr.insertCell();
-    td1.appendChild(document.createTextNode(obj[key]))
-})
-
-setTimeout(() => {
-document.location.reload();
-}, 3000);
-
-body.appendChild(tbl);
-console.log("TEST")
