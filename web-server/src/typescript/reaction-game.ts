89ef35d2b3d82a536a87b26c57bb65b23d1533ca
Nikolas Paripovic <nikolas.paripovic@esrlabs.com>
2023-05-24 16:45:21 +0200

    Implement reaction game mechanic

--- web-server/src/typescript/reaction-game.ts
+++ web-server/src/typescript/reaction-game.ts
@@ -1,43 +1,94 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const startBtn = document.getElementById('start-btn');
-  const counter = document.getElementById('counter');
-  const signal = document.getElementById('signal');
+import di from "./di.js";
+import { BackendService } from "./backend.service.js";
 
-  startBtn.addEventListener('click', startGame);
+interface Leaderboard {
+  entries: LeaderboardEntry[],
+  droppedOutButtonIndices: number[],
+}
 
-  function startGame() {
-    startBtn.disabled = true;
-    counter.classList.remove('d-none');
-    countdown(3, go);
+interface LeaderboardEntry {
+  buttonIndex: number,
+  reactionTimeMs: number,
+}
+
+
+class ReactionGameComponent {
+
+  private _startButtonElement: HTMLButtonElement;
+
+  private _counterElement: HTMLDivElement;
+
+  private _pressButtonSignalElement: HTMLDivElement;
+
+
+  constructor(private _backendService: BackendService) { };
+
+  onInit() {
+    this._startButtonElement = document.getElementById('start-btn') as HTMLButtonElement;
+    this._counterElement = document.getElementById('counter') as HTMLDivElement;
+    this._pressButtonSignalElement = document.getElementById('signal') as HTMLDivElement;
+
+    this._startButtonElement.addEventListener('click', _ => this.onClickStartButton());
+
+    const actionSocket = new WebSocket('ws://localhost:8000/reaction-game/action');
+    const leaderboardSocket = new WebSocket('ws://localhost:8000/reaction-game/leaderboard');
+
+    actionSocket.addEventListener('message', _ => this.onReactionPhaseStarts());
+    leaderboardSocket.addEventListener('message', event => {
+      const json = event.data;
+      const leaderboard = JSON.parse(json) as Leaderboard;
+      return this.onLeaderboardChanges(leaderboard);
+    });
+
   }
 
-  function countdown(time, callback) {
-    counter.innerText = time;
+  private onClickStartButton() {
+    this.startGame();
+  }
+
+  private onCountdownEnds() {
+    this._backendService.startReactionGame();
+  }
+
+  private onReactionPhaseStarts() {
+    this._pressButtonSignalElement.innerText = 'React now!';
+    this._pressButtonSignalElement.classList.add('bg-danger', 'text-white');
+    this.endGame();
+  }
+
+  private onLeaderboardChanges(leaderboard: Leaderboard) {
+    console.log(leaderboard);
+  }
+
+
+
+  private startGame() {
+    this._startButtonElement.disabled = true;
+    this._pressButtonSignalElement.classList.add('d-none');
+    this.setCountdown(3, () => this.onCountdownEnds());
+  }
+
+  private endGame() {
+    this._startButtonElement.disabled = false;
+  }
+
+  private setCountdown(count: number, callback: () => void) {
+
+    this._counterElement.classList.remove('d-none'); // show the counter
+    this._counterElement.innerText = count.toString();
     const countdownInterval = setInterval(() => {
-      time--;
-      counter.innerText = time;
-      if (time === 0) {
+      count--;
+      this._counterElement.innerText = count.toString();
+      if (count < 0) {
         clearInterval(countdownInterval);
+        this._counterElement.classList.add('d-none'); // Hide the counter
         callback();
       }
     }, 1000);
+
   }
 
-  function go() {
-    signal.classList.remove('d-none');
-    const randomDelay = Math.floor(Math.random() * 5000);
-    setTimeout(() => {
-      signal.innerText = 'React now!';
-      signal.classList.add('bg-danger', 'text-white');
-      document.addEventListener('click', reactionTime);
-    }, randomDelay);
-  }
+}
 
-  function reactionTime() {
-    const reactionTime = Date.now();
-    document.removeEventListener('click', reactionTime);
-    signal.innerText = `Reaction time: ${reactionTime}ms`;
-    signal.classList.remove('bg-danger', 'text-white');
-    startBtn.disabled = false;
-  }
-});
+const reactionGameComponent = new ReactionGameComponent(di.backendService);
+document.addEventListener('DOMContentLoaded', () => reactionGameComponent.onInit());
